38decfb8aca987d9357eab5f3b01c76cba8539c3
Malakondaiah-Gola <155671084+Malakondaiah-Gola@users.noreply.github.com>
2024-01-06 01:47:01 +0530

    Delete src/main/java/com/capstone directory

--- src/main/java/com/capstone/pages/CartPage.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.pages;
-
-import org.openqa.selenium.By;
-
-public class CartPage {
-
-	public static final By CART_SUCCESS_MESSAGE = By.xpath("//a[contains(text(),'MacBook AIR Apple M2')]");
-
-}
